11ec5e5e4ed533da0ba4d309aef5bd30041d8884
nihalupreti <n.ytuchiha@gmail.com>
2024-12-08 14:00:01 +0545

    optimised task routes

--- src/middlewares/errors.js
+++ src/middlewares/errors.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-  res.status(err.statusCode || 500).json({
+  return res.status(err.statusCode || 500).json({
     status: err.status,
     error: {
       message: err.message || "Something went wrong!",
--- src/routes/task.js
+++ src/routes/task.js
@@ -1,97 +1,88 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/auth");
 const { todos: task } = require("../config/database");
+const sendSuccessResponse = require("../utils/reponse");
+const ApiError = require("../utils/customError");
 
 const router = express.Router();
 
-router.post("/todo", authMiddleware, (req, res) => {
+router.post("/todo", authMiddleware, async (req, res, next) => {
   const { title, description, dueDate, status } = req.body;
 
-  const todo = new task({
-    title: title,
-    description: description,
-    dueDate: dueDate,
-    status: status,
-    user: req.user.userid,
-  });
-  todo
-    .save()
-    .then((data) => {
-      console.log("sucessfully saved the todo.");
-      res.status(200).json(data);
-    })
-    .catch((err) => {
-      console.log("could not save the todos.");
-      res.status(400).json({
-        message: "something went wrong while trying to save to the database",
-      });
+  try {
+    const todo = new task({
+      title: title,
+      description: description,
+      dueDate: dueDate,
+      status: status,
+      user: req.user.userid,
     });
+    const saveTodo = await todo.save();
+    sendSuccessResponse(res, 200, saveTodo, "Sucessfully created the task.");
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get("/todos", authMiddleware, (req, res) => {
-  task
-    .find({ user: req.user.userid })
-    .then((task) => {
-      res.status(200).json(task);
-    })
-    .catch((err) => {
-      console.log("couldnot fetch tasks from database.");
-      res.status(400).json({ message: "try again" });
-    });
+router.get("/todos", authMiddleware, async (req, res, next) => {
+  try {
+    const allUserTasks = await task.find({ user: req.user.userid });
+    sendSuccessResponse(res, 200, allUserTasks, "All tasks.");
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete("/todo/:id", authMiddleware, (req, res) => {
+router.delete("/todo/:id", authMiddleware, async (req, res, next) => {
   const { id } = req.params;
-  task
-    .findByIdAndDelete({ _id: id, user: req.user.userid })
-    .then((deletedTask) => {
-      if (!deletedTask) {
-        return res
-          .status(404)
-          .json({ message: "Task not found or not authorized" });
-      }
-      res.status(200).json(deletedTask);
-    })
-    .catch((err) => {
-      console.error("Error deleting task:", err);
-      res
-        .status(500)
-        .json({ message: "An error occurred while deleting the task" });
+  try {
+    const deletedTask = await task.findByIdAndDelete({
+      _id: id,
+      user: req.user.userid,
     });
+
+    if (!deletedTask) {
+      throw new ApiError(404, "Task not found.", `No task with id ${id}`);
+    }
+
+    sendSuccessResponse(res, 200, deletedTask, "Task deleted successfully.");
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.put("/todo/:id", authMiddleware, (req, res) => {
-  const { title, description, dueDate, status } = req.body; //updated data
-  const { id } = req.params;
-  task
-    .findByIdAndUpdate(
-      id, //id of tasks to be updated
-      { title, description, dueDate, status }, // New data to update
-      { new: true } // Returns the updated task
-    )
-    .then((updatedTask) => {
-      if (!updatedTask) {
-        return res.status(404).json({ message: "Task not found" });
-      }
-      res
-        .status(200)
-        .json({ message: "Task updated successfully", task: updatedTask });
-    })
-    .catch((err) => {
-      console.log("Error updating task:", err);
-      res.status(500).json({ message: "Server error" });
-    });
-});
+// router.put("/todo/:id", authMiddleware, (req, res) => {
+//   const { title, description, dueDate, status } = req.body; //updated data
+//   const { id } = req.params;
+//   task
+//     .findByIdAndUpdate(
+//       id, //id of tasks to be updated
+//       { title, description, dueDate, status }, // New data to update
+//       { new: true } // Returns the updated task
+//     )
+//     .then((updatedTask) => {
+//       if (!updatedTask) {
+//         return res.status(404).json({ message: "Task not found" });
+//       }
+//       res
+//         .status(200)
+//         .json({ message: "Task updated successfully", task: updatedTask });
+//     })
+//     .catch((err) => {
+//       console.log("Error updating task:", err);
+//       res.status(500).json({ message: "Server error" });
+//     });
+// });
 
-router.get("/todo/completed", authMiddleware, (req, res) => {
-  task
-    .find({ user: req.user.userid, status: true })
-    .then((task) => {
-      res.status(200).json(task);
-    })
-    .catch((err) => {
-      console.log("couldnot fetch completed tasks from database.");
-      res.status(400).json({ message: "try again" });
-    });
-});
+// router.get("/todo/completed", authMiddleware, (req, res) => {
+//   task
+//     .find({ user: req.user.userid, status: true })
+//     .then((task) => {
+//       res.status(200).json(task);
+//     })
+//     .catch((err) => {
+//       console.log("couldnot fetch completed tasks from database.");
+//       res.status(400).json({ message: "try again" });
+//     });
+// });
 module.exports = router;
